d1586795b90e6e3e6f33039431e15c2d74b523e9
Derrickas <Derrickas@outlook.com>
2020-01-19 22:03:08 -0600

    add color to textbox

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-const hours = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+
+$(document).ready(function() {
 
 
 
-$('.hourblocks')
-$('button')
+
+// const hours = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+
+// $('.form-control').on(b)
+$('.form-control').focus(function(){
+   $(this).css('background', 'green');
+
+});
+
+$(this).keyup(function(e){
+    console.log(e.target.value);
+});
+$('.btn').submit(function(e) {
+    e.preventDefault();
+    var input = $('.form-control').val();
+    console.log(input);
+})
+
+
+// function contentCheck()
+// function changeColor()
+
+
+// window.localStorage.setItem( "agendaitem", Userinput)
+    
+
+//     $(".btn").click(function() {
+//        console.log("#input").text
+//         // $("input").append(".form-control")
+//     }) 
+})
